68ef8f7b890e523f9fdef06b60d64b5f80735524
irekgroup <ireknizamov94@gmail.com>
2024-08-19 23:47:01 +0300

    feat: Integrate Redis caching and prepare for Strapi Cloud deployment
    - Configured Redis caching for the 'article' model in Strapi.
    - Set up Docker to run Redis locally for development.
    - Updated Strapi configuration to connect to Redis in a Docker container.
    - Verified and tested Redis integration with Strapi locally.
    - Prepared the project for deployment to Strapi Cloud with Redis support.

--- config/middleware.js
+++ config/middleware.js
@@ -5,9 +5,9 @@ module.exports = ({ env }) => ({
       type: 'redis',
       maxAge: 3600000, // 1 hour in milliseconds
       redisConfig: {
-        host: env('REDIS_HOST', null), // Подключаемся к Redis, работающему в Docker
-        port: env('REDIS_PORT', null),         // Порт по умолчанию для Redis
-        password: env('REDIS_PASSWORD', null), // Если у вас настроен пароль для Redis, укажите его здесь
+        host: env('REDIS_HOST', 'redis-19991.c8.us-east-1-4.ec2.redns.redis-cloud.com'), // Хост вашего Redis-сервера
+        port: env.int('REDIS_PORT', 19991),  // Порт вашего Redis-сервера
+        password: env('REDIS_PASSWORD', 'lahYVIhHyvDj8Uvr6JrOGzRIpwQW0yqq'), // Пароль для доступа к Redis
       },
       models: [
         'article', // Кеширование модели article
